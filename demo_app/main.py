97476636095d9fbf37da17817cd80aed6bc7ad21
Zhi Li <lzblack@users.noreply.github.com>
2025-10-28 21:24:58 -0400

    Finish session 08

---
+++ demo_app/main.py
@@ -0,0 +1,5 @@
+import utils
+
+print(f'Value of PI: {utils.PI}')
+
+print(utils.PI*2)  # Example usage of PI in a calculation
--- hello.py
+++ hello.py
@@ -1,7 +1,25 @@
-print("Hello, world!")
+# print("Hello, world!")
 
 
-print(39 + 41 + 33)
+# print(39 + 41 + 33)
 
 
-print(2 ++ 2) # it does not have any effect
+# print(2 ++ 2) # it does not have any effect
+
+
+name = input("What is your name?")
+
+# print("Hello, " + name + "!")
+print(f'Hello, {name}!')
+
+
+# print('What can I do for you, ' + name + '?')
+print(f'What can I do for you, {name}?')
+
+
+# n = 42
+# n + 10 # the expression has no effect
+
+# x = n * 2
+# print(x)
+
